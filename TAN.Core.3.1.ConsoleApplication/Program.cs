bc042a8c396a0c35210b5c5ba241dbc0c4f743b4
Thiago Autran <thiago.autran@gmail.com>
2021-03-09 09:23:08 -0300

    .

---
+++ TAN.Core.3.1.ConsoleApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TAN.Core._3._1.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
